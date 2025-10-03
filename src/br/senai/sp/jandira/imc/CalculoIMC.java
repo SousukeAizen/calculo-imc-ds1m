265ac143ecb81f1b3646d3e96064510f9e2818a8
SousukeAizen <henriquealves5590@gmail.com>
2025-10-03 17:29:07 -0300

    Atualização: ajustes no código do IMC

---
+++ src/br/senai/sp/jandira/imc/CalculoIMC.java
@@ -0,0 +1,19 @@
+import br.senai.sp.jandira.imc.model.IMC;
+
+public class CalculoIMC {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando o Programa...");
+
+        IMC imc = new IMC();
+        imc.obterDados();
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/br/senai/sp/jandira/imc/model/IMC.java
@@ -0,0 +1,104 @@
+package br.senai.sp.jandira.imc.model;
+
+import java.util.Scanner;
+
+public class IMC {
+
+    String nomeDoAluno;
+    double pesoDoAluno;
+    double alturaDoAluno;
+    double imc;
+    String status;
+
+
+
+
+    public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Qual o nome do Aluno:  ");
+        nomeDoAluno = leitor.nextLine();
+
+        System.out.print("Qual o peso do Aluno (ex: 90Kg): ");
+        String pesoDigitado = leitor.nextLine().replace(",", ".").replaceAll("[^0-9.]", "");
+        pesoDoAluno = Double.parseDouble(pesoDigitado);
+
+        System.out.print("Qual a altura do Aluno (ex: 1.80m): ");
+        String alturaDigitada = leitor.nextLine().replace(",", ".").replaceAll("[^0-9.]", "");
+        alturaDoAluno = Double.parseDouble(alturaDigitada);
+
+
+
+        calcularDados();
+
+
+
+
+
+    }
+
+    public void calcularDados() {
+        imc = pesoDoAluno / (alturaDoAluno * alturaDoAluno);
+
+        if (imc < 18.5) {
+            status = "abaixo do peso";
+        } else if (imc >= 18.6 && imc <= 24.9) {
+            status = "Peso ideal (parabéns)";
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            status = "Levemente acima do peso";
+        } else if (imc >= 30.0 && imc <= 34.9) {
+            status = "Obesidade grau I";
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            status = "Obesidade grau II (severa)";
+        } else {
+            status = "Obesidade III (mórbida)";
+        }
+
+
+        exibirResultados();
+
+
+    }
+
+    public void exibirResultados(){
+        System.out.println("\n--- RESULTADO ---");
+        System.out.println("Aluno: " + nomeDoAluno);
+        System.out.println("Peso: " + pesoDoAluno + " kg");
+        System.out.println("Altura: " + alturaDoAluno + " m");
+        System.out.printf("IMC Calculado: %.2f\n", imc);
+        System.out.println("Status: " + status);
+        System.out.println("---");
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
